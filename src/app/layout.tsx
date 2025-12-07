943c05ff593b709ab249b1ca495adbf8d5c26ac7
Sri Swaroop Kumar Akili <joyboysskofficially@gmail.com>
2025-12-07 17:43:23 +0000

    Try fixing this error: `Console SecurityError: Failed to read the 'cssRu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
@@ -26,6 +27,7 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
           rel="stylesheet"
+          crossOrigin="anonymous"
         />
       </head>
       <body className={cn("font-body antialiased min-h-screen")}>
